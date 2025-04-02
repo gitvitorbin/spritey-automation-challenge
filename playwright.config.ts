82030f7eb6b03a22a0caa4cbcc7e10f6522dacdf
gitvitorbin <gitvitorbin@gmail.com>
2025-04-02 10:21:18 -0300

    moved headless command to playwirght config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         ...devices['Desktop Chrome'],
         baseURL: 'https://www.saucedemo.com',
         trace: 'on-first-retry',
+        headless: true,
       },
     },
     /*
